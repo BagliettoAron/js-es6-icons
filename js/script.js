62d7e7a36967aadac903e7edc61d707f07379fcd
@BagliettoAron <aronbaglietto88@gmail.com>
2022-05-02 15:28:11 +0200

    Add: Milestone 1 & Milestone 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
-// Milestone 1
-// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
-// Milestone 2
-// Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
 
 
 
 
 
-[
+
+animalsArray = [
+
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -121,3 +119,33 @@
 		color: 'blue'
 	}
 ];
+
+// Milestone 1
+// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+
+// Milestone 2
+// Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+
+// estraggo gli elementi necessari a formare il nome della icon e gli concateno
+animalsArray.forEach((animalData) => {
+    const thisAnimalIcon = `${animalData.family} ${animalData.prefix}${animalData.name}`
+    console.log(thisAnimalIcon);
+    
+    // aggancio il main-container
+    const cardWrapper = document.querySelector(".main-container")
+    console.log(cardWrapper);
+
+    // aggiungo l'html
+    cardWrapper.innerHTML += (
+        `<div class="card-wrapper">
+            <div class="card-content">
+                <i class="${thisAnimalIcon} ${animalData.color}"></i>
+                <h4>${animalData.name}</h4>
+            </div>
+        </div>`
+    )
+})
+
+
+
+
